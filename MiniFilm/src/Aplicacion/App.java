ba2b13809dcfad1cfec153d554cde9a6836eb1cd
al-waseem <waseem_khalel@hotmail.com>
2014-05-02 14:24:37 +0200

    comentar el archivo App.java

--- MiniFilm/src/Aplicacion/App.java
+++ MiniFilm/src/Aplicacion/App.java
@@ -7,18 +7,31 @@ import ventana.Film;
 
 public class App {
 	
+	//crear un objeto de tipo Film (ventana)
 	private Film film;
 	
+	//constructor de la aplicacion
 	App(){
+		
+		//instanciar el objeto
 		film = new Film();
+		//asignar un titulo de la ventana
 		film.setTitle("Mni Film");
+		//cerrar la aplicacion al cerrar la ventana
 		film.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//asignar el tamaño de la ventana
 		film.setSize(500,500);
+		//mostrar la ventana
 		film.setVisible(true);
+		
+		/*metodo para centrar la ventana en la pantalla
+		 * utilizando una libreria hecha para esto
+		 */
 		windows.Window.centrar(film);
 	}
 	
 	public static void main(String[] args) {
+		//crear la aplicacion para ejecutarla
 		new App();
 	}
 
